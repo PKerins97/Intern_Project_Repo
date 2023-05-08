7ae2f93e089341452a79a27144fb2e04e12ac0ca
PKerins97 <paulkerins123@gmail.com>
2023-05-08 16:10:11 +0100

    django installed

--- Shepherd_project/Shepherd/forms.py
+++ Shepherd_project/Shepherd/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User 
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model=User
         fields = ['username','first_name','last_name','email','password1','password2']
+
+        def clean_email(self):
+            email= self.clean_data["email"]
+            if User.objects.filter(email=email).exists():
+                raise ValidationError("A user with this email already exists!")
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=65)
