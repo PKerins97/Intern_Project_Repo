06dd6658574eba8d614b3f9be11c9b13bd28237b
PKerins97 <paulkerins123@gmail.com>
2023-05-18 11:31:05 +0100

    created code for daily login & daily login model

--- Shepherd_project/Shepherd/models.py
+++ Shepherd_project/Shepherd/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Points(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     points = models.IntegerField()
+
+class DailyLogin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    login_date = models.DateField(auto_now_add=True)
+    points = models.ForeignKey(Points,on_delete=models.CASCADE)
--- Shepherd_project/Shepherd/views.py
+++ Shepherd_project/Shepherd/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.template import loader
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from datetime import date
 
 from .forms import LoginForm, RegisterForm
 from .models import *
@@ -16,7 +18,7 @@ def home(request):
     template = 'home.html'
     context = {
         'user' : request.user,
-        'mypoints': Points.objects.get(user_id=request.user.id).points
+        #'mypoints': Points.objects.get(user_id=request.user.id).points
     }
     return  render(request, template, context)
 
@@ -41,12 +43,11 @@ def login(request):
                 djlogin(request, user)
                 if not remember_me:
                     request.session.set_expiry(0)
-                    return redirect ('home')
                 else:
                     request.session.set_expiry(1209600)
-            return redirect('login')
+            return redirect('home')
 
-        return render(request, 'login.html',{'form' : form})
+        return render(request, 'login.html',{'form' : form, 'user': request.user})#request user passes user to the home page 
     
 def logout(request):
     djlogout(request)
@@ -74,3 +75,21 @@ def UserLoggedIn(request):
     else:
         username = None
     return username
+
+@login_required
+def login_view(request):
+    # Check if the user already has a daily login record for today
+    today = date.today()
+    daily_login = DailyLogin.objects.filter(user=request.user, login_date=today).first()
+
+    if not daily_login:
+        # User hasn't logged in today, create a new daily login record
+        daily_login = DailyLogin(user=request.user, login_date=today)
+        daily_login.save()
+
+        # Give rewards to the user
+        daily_login.points += 1  # Add a point for each daily login
+        daily_login.save()
+
+    # Pass the daily login record to the template for display
+    return render(request, 'login.html', {'daily_login': daily_login})
