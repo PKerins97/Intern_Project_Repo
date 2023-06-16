a2338e733945047885db19e316474c0eb2d4cd17
Nikita Dmtriev <nikita.dmitriev@ucdconnect.ie>
2023-06-16 17:11:04 +0100

    Add model

--- Shepherd_project/Shepherd/models.py
+++ Shepherd_project/Shepherd/models.py
@@ -13,4 +13,10 @@ class DailyLogin(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     points = models.IntegerField(default=0)
     date_added = models.DateTimeField(default=timezone.now)
+    
+def user_post_path(instance, filename):
+    return "media/posts/{0}/{1}".format(instance.user_posted.username, filename)
    
+class Post(models.Model):
+    user_posted = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=user_post_path) # pip install pillow
