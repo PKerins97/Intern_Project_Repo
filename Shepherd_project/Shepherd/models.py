f58b586a6a31451f0fa041a3cd4b27f666e0f69c
PKerins97 <paulkerins123@gmail.com>
2023-07-12 13:57:24 +0100

    updated item model

--- Shepherd_project/Shepherd/models.py
+++ Shepherd_project/Shepherd/models.py
@@ -26,7 +26,7 @@ class Items(models.Model):
     store = models.CharField(max_length=100)
     price = models.FloatField()
     price_now = models.FloatField(blank=True,null = True)
-    image = models.ImageField(upload_to='static/img')
+    image = models.ImageField(upload_to='static')
 
     
     
